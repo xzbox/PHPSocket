6869fb995b61b932662a2c68a9965bf47343d66a
Qti3e <Alirezaghadimy@gmail.com>
2016-05-05 13:02:27 +0430

    Disable auto socket_addr

--- server/config.php
+++ server/config.php
@@ -24,7 +24,8 @@ if(class_exists('\lib\network\Network')){
 	 * As you now for set bind a socket we have to set a listening address, this constant is socket's listen address.
 	 * You can change it your custom configure.
 	 */
-	define('socket_addr',\lib\network\Network::ServerIPv4());
+	//define('socket_addr',\lib\network\Network::ServerIPv4());
+	define('socket_addr','127.0.0.1');
 }else{
 	/**
 	 * This is socket's port for times that system is not connected to any network and there is no IPV4.
@@ -53,7 +54,7 @@ if(class_exists('\lib\network\Network')){
 	/**
 	 * In PHPSocket like PHP we have tmp folder for saving sessions' data
 	 */
-	define('sessions_folder','.tmp');
+	define('sessions_folder','server/.tmp');
 
 	/**
 	 * It's nothing :)
