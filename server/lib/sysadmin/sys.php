4b5c49c2c9cb013306df80a7c12931bba9049ab0
Qti3e <Alirezaghadimy@gmail.com>
2016-06-05 14:27:35 +0430

    First update

--- server/lib/sysadmin/sys.php
+++ server/lib/sysadmin/sys.php
@@ -20,10 +20,72 @@
  *****************************************************************************/
 namespace lib\sysadmin;
 
+use lib\client\sender;
+
 /**
  * Class sys
  * @package lib\sysadmin
  */
 class sys{
+	private static $user;
+	/**
+	 * @param $user
+	 * @param $message
+	 *
+	 * @return void
+	 */
+	public static function onMessage($user,$message){
+		self::$user = $user;
+		preg_match_all("/[a-z]+(\\s+'[\\w\\W]*?'|\\s+[0-9]+)*/",$message,$commands);
+		$commands   = $commands[0];
+		$count      = count($commands);
 
+		$ex         = explode(' ',$commands[0]);
+		$in         = substr($commands[0],strlen($ex[0])+1);
+		preg_match_all("/'([\\w\\W]*?)'|([^\\s']+)/",$in,$p);
+		$input      = self::runCommand($ex[0],$p[1]);
+
+		for($i = 1;$i < $count;$i++){
+			$ex         = explode(' ',$commands[$i]);
+			$in         = substr($commands[$i],strlen($ex[0])+1);
+			preg_match_all("/'([\\w\\W]*?)'|([^\\s']+)/",'2 '.$in,$p);
+			$p[1][0]    = $input;
+			$input      = self::runCommand($ex[0],$p[1]);
+		}
+		sender::ByUser($user,$input);
+	}
+
+	/**
+	 * @param $name
+	 *
+	 * @return bool
+	 */
+	protected static function commandExists($name){
+		return class_exists('lib\\sysadmin\\commands\\'.$name);
+	}
+
+	/**
+	 * @param $name
+	 * @param $param
+	 *
+	 * @return bool|string
+	 */
+	public static function runCommand($name,$param){
+		$count = count($param);
+		if(self::commandExists($name)){
+			$class  = 'lib\\sysadmin\\commands\\'.$name;
+			if($class::$needLogin == true && self::$user->isAdmin == 1){
+				return false;
+			}else{
+				for($i = 0;$i < $count;$i++){
+					if(($param[$i][0] == substr($param[$i],-1)) == '\''){
+						$param[$i]    = substr($param[$i],1,-1);
+					}
+				}
+				return $class::process($param,self::$user);
+			}
+		}else{
+			return '!Command not found';
+		}
+	}
 }
